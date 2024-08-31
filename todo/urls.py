9cabba282fe59fb8800e70f1654bf0991996dbbd
manyajain27 <jainmanya2701@gmail.com>
2024-08-31 18:21:28 +0530

    added logout button

--- todo/urls.py
+++ todo/urls.py
@@ -13,12 +13,8 @@ urlpatterns = [
     path('task/<int:pk>/edit/', views.update_task, name='task_update'),
     path('task/<int:pk>/delete/',views.delete_task, name='task_delete'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name='register'),
-    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
